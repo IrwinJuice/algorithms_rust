2beb7a02b26dd4af8bbab95827a5c5d349cced81
Maksym Sevriukov <sevriukovmk@gmail.com>
2023-06-16 21:45:56 +0300

    leetcode: is_anagram

--- src/leetcode/valid_anagram.rs
+++ src/leetcode/valid_anagram.rs
@@ -25,6 +25,6 @@ use std::collections::HashMap;
 pub fn is_anagram(s: String, t: String) -> bool {
     let mut m = HashMap::new();
     s.chars().for_each(|c| *m.entry(c).or_insert(0) += 1);
-    s.chars().for_each(|c| *m.entry(c).or_insert(0) -= 1);
+    t.chars().for_each(|c| *m.entry(c).or_insert(0) -= 1);
     m.into_values().all(|v|v == 0)
 }
